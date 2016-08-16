1b50f66acc8ce8144d7657c274396bf1f35ae7fa
Kirill <c.derche@me.com>
2016-08-17 00:40:19 +0200

    Merge branch 'master' into passportjs
    # Conflicts:
    #	api/policies/localize.js

--- api/policies/localize.js
+++ api/policies/localize.js
@@ -1,12 +1,5 @@
 module.exports = function(req, res, next) {
-<<<<<<< HEAD
-  // console.log('localize', req.getLocale());
-  if ((req.cookies.preferredLanguage != req.getLocale()) && (req.cookies.preferredLanguage != null)) {
-=======
   if (req.cookies.preferredLanguage && (req.cookies.preferredLanguage != req.getLocale()))
->>>>>>> master
     req.setLocale(req.cookies.preferredLanguage)
-    // console.log('New locale', req.getLocale());
-  }
   next()
 };
