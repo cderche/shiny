4cb7de00be3ebc2b238b1523b1def23be5225357
Kirill <c.derche@me.com>
2016-08-17 00:06:06 +0200

    testing locale issue.

--- api/policies/localize.js
+++ api/policies/localize.js
@@ -1,4 +1,6 @@
 module.exports = function(req, res, next) {
+  console.log('req.cookies.preferredLanguage', req.cookies.preferredLanguage);
+  console.log('req.getLocale()', req.getLocale());
   if (req.cookies.preferredLanguage != req.getLocale())
     req.setLocale(req.cookies.preferredLanguage)
   next()
