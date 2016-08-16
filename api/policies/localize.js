4adfe0826da39af08939a0fb4a9e768a1ce32d83
Kirill <c.derche@me.com>
2016-08-17 00:09:22 +0200

    more tests.

--- api/policies/localize.js
+++ api/policies/localize.js
@@ -1,7 +1,10 @@
 module.exports = function(req, res, next) {
   console.log('req.cookies.preferredLanguage', req.cookies.preferredLanguage);
   console.log('req.getLocale()', req.getLocale());
-  if (req.cookies.preferredLanguage != req.getLocale())
+  if (req.cookies.preferredLanguage && (req.cookies.preferredLanguage != req.getLocale())) {
+    console.log('Before set');
     req.setLocale(req.cookies.preferredLanguage)
+    console.log('After set');
+  }
   next()
 };
