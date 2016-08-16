c86788a8a028851bdf063f36468ec5b0c50338c2
Kirill <c.derche@me.com>
2016-08-17 00:12:01 +0200

    Fix on localize policy

--- api/policies/localize.js
+++ api/policies/localize.js
@@ -1,10 +1,5 @@
 module.exports = function(req, res, next) {
-  console.log('req.cookies.preferredLanguage', req.cookies.preferredLanguage);
-  console.log('req.getLocale()', req.getLocale());
-  if (req.cookies.preferredLanguage && (req.cookies.preferredLanguage != req.getLocale())) {
-    console.log('Before set');
+  if (req.cookies.preferredLanguage && (req.cookies.preferredLanguage != req.getLocale()))
     req.setLocale(req.cookies.preferredLanguage)
-    console.log('After set');
-  }
   next()
 };
